3f04d1ab884bd9451d5085633a4e00851b6c38f2
Shubham <ssp671995@gmail.com>
2022-05-04 12:54:54 +0530

    Adding code to set event listenere to salary and name

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -22,3 +22,44 @@ window.addEventListener('DOMContentLoaded', (event) =>{
     });
     
     });
+    
+    
+    const save = () => {
+        let employeePayrollData;
+        try{
+            employeePayrollData = createEmployeePayroll();
+        }catch(e){
+            return;
+        }
+    }
+    
+    const createEmployeePayroll = () => {
+        let employeePayrollData = new EmployeePayRollData();
+        employeePayrollData.name = getInputValueById('#name');
+        employeePayrollData.profilePic = getSelectedValues('[name=profile]').pop();
+        employeePayrollData.gender = getSelectedValues('[name=gender]').pop();
+        employeePayrollData.department = getSelectedValues('[name=department]');
+        employeePayrollData.salary = getInputValueById('#salary');
+        employeePayrollData.note = getInputValueById('#notes');
+        let year = getInputValueById('#year');
+        let month = parseInt(getInputValueById('#month'))-1;
+        let day = getInputValueById('#day');
+        employeePayrollData.startDate = new Date(year,month,day);
+        alert(employeePayrollData.toString());
+        return employeePayrollData;
+    }
+    
+    const getInputValueById = (id) => {
+        let value = document.querySelector(id).value;
+        return value;
+    }
+    
+    const getSelectedValues = (propertyValue) => {
+        let allItems = document.querySelectorAll(propertyValue);
+        let selItems = [];
+        allItems.forEach(item => {
+            if(item.checked)
+            selItems.push(item.value);
+        });
+        return selItems;
+    }
